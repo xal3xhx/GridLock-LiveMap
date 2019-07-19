599d8ff5b8c1a27b4aa83c321bdaefc42fe743bb
alex <mralexh123@gmail.com>
2019-07-19 00:25:43 -0400

    added ups and player count

--- include/api.php
+++ include/api.php
@@ -53,7 +53,7 @@ function runCommandTest($command, $token, $nodeId = null) {
 <?php
  $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 0);
+    curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_VERBOSE, 1);
     curl_setopt($ch, CURLOPT_HEADER, 1);
--- index.php
+++ index.php
@@ -17,23 +17,29 @@
 	<body>
 		<?php
 			//echo("test");
-			//print_r($responce);
+			
 			//echo("test");
 			foreach($slaves as $slave) {
 				if(isset($slave['info'])) {
+					//print_r($slave);
 					print_r($slave['instanceName']);
 					echo(' @ '.$slave['publicIP'].':'.$slave['serverPort']."\r\n<br />Status: ");
 					$online = is_resource(@fsockopen($slave['publicIP'], $slave['rconPort']));
 					if($online) {
 						echo('<font style="color: green">Online</font><br />');
-						echo($slave['UPS']);
-						//echo('<a href="steam://rungameid/427520//--mp-connect='.$slave['publicIP'].':'.$slave['serverPort'].'">Click here</a> to connect to the server (only works if you have Factorio on Steam)<br />'."\r\n");
+						echo('<a href="steam://rungameid/427520//--mp-connect='.$slave['publicIP'].':'.$slave['serverPort'].'">Click here</a> to connect to the server (only works if you have Factorio on Steam)<br />'."\r\n");
+						echo("UPS: ");
+						echo($slave['meta']['UPS'] . "<br />\r\n");
 						$rconResult = rconCommand($slave['publicIP'], $slave['rconPort'], $slave['rconPassword'], '/p o');
 						if(!$rconResult->err) {
 							echo($rconResult->msg."<br />\r\n");
 						}
-					} else {
-						echo('<div style="color: red">Offline</div>');
+						else {
+							echo ($rconResult->msg);
+						}
+					} 
+					else {
+						echo('<font style="color: red">Ofline</font><br />');
 					}
 					echo("\r\n<br />\r\n");
 					//print_r($slave['info']);
